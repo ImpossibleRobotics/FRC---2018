0f2fad05f34f819841ca5db085a219ef503b03f4
Dylan Vos <dylan@vossystems.nl>
2018-02-17 14:00:16 +0100

    Updated IRDriverOne
    Changed ArcadeDrive to TankDrive

--- src/IRDriverOne.cpp
+++ src/IRDriverOne.cpp
@@ -19,7 +19,8 @@ IRDriverOne::~IRDriverOne() {
 }
 
 void IRDriverOne::Run(){
-	myDrive->ArcadeDrive(joystick, true); // drive with arcade style (use right stick), boolean true if using deadZone
+//	myDrive->ArcadeDrive(joystick, true); // drive with arcade style (use right stick), boolean true if using deadZone
+	myDrive->TankDrive(joystick, true); // drive with controller tank style (use left stick for left and right stick for right), boolean true if using deadZone
 
 	frc::Wait(0.005);
 }
